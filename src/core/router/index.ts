05a1090d8d8c2bde993f86ad5fff988ee493ae5b
angel_dev <angelguille1@hotmail.com>
2025-07-11 02:04:53 -0600

    7112025

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -3,29 +3,38 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
+    path: "/login",
+    name: "login",
+    meta: { requiresAuth: false, title: "Iniciar sesion" },
+    component: () => import("../../modules/auth/views/Login.vue")
+  },
+  {
     path: "/",
     name: "layout",
     component: () => import("../layouts/Layout.vue"),
-    meta: { requiresAuth: false},
+    meta: { requiresAuth: false },
     children: [
       {
         path: "/test-view",
         name: "test-view",
-        component: () => import("../components/AppTestComponents.vue")
-      }
-    ]
+        component: () => import("../components/AppTestComponents.vue"),
+      },
+    ],
   },
   {
     path: "/forbidden",
     name: "forbidden",
     meta: { requiresAuth: false, title: "Acceso denegado" },
-    component: () => import("../views/Forbidden.vue"),
+    component: () => import("../../views/Forbidden.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition){
+    return {top: 0}
+  }
 });
 
 export default router;
---
+++ src/modules/auth/views/Login.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+    <h1>Este sera el login</h1>
+</div>
+</template>
+<script setup lang="ts">
+</script>
