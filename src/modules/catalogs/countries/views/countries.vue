8f22bfafc824dca886430c52026b0fc67bfa327b
henrygutierrez23 <henryivang@gmail.com>
2025-09-19 15:57:19 -0600

    feat:avances

--- src/modules/catalogs/countries/views/countries.vue
+++ src/modules/catalogs/countries/views/countries.vue
@@ -23,7 +23,7 @@
         </Button>
 
         <Button
-          @click="openModalCreate(showModal)"
+          @click="openModal('create')"
           icon="pi pi-plus"
           class="flex w-10 h-10 rounded-full p-0 sm:ml-auto sm:mr-0 mx-auto">
         </Button>
@@ -31,42 +31,45 @@
         <AppModal
           :show="showModal"
           show-icon-close
-          title="Agregar País"
+          :title="isMode ? 'Editar País' : 'Agregar País'"
           @close-modal="closeModalCreate"
           @confirm-modal="confirmModal"
           width="350px"
         >
           <div class="flex flex-col gap-6 py-5 w-[230px]">
             <AppInputText 
-              v-model="newCountryName"
+              v-model="tempCountry.nombre"
               class="lg:w-full grow sm:max-w-[500px]"
               label="Ingrese el nombre del país"
             />
             <AppInputText 
-              v-model="newCountryAbreviation"
+              v-model="tempCountry.abreviación"
               class="lg:w-full grow sm:max-w-[500px]"
               label="Ingrese la abreviación del país"
             />
 
             <AppInputText 
-              v-model="newCountryCode"
+              v-model="tempCountry.código"
               class="lg:w-full grow sm:max-w-[500px]"
               label="Ingrese el código del país"
             />
           </div>
         </AppModal>
 
+
         <AppDataTable
           :items="items"
           :headers="headers"
           :paginator="true"
-          :per_page="1"
+          :per_page="10"
           :total_pages="1"
           class="w-full"
         >
-
-
-        
+        <template #body-vinculador_seis="{ data }">
+          <i class="pi pi-pencil cursor-pointer hover:text-[#082f49] transition-colors" @click="openModal('edit', data)"></i>
+          <i class="pi pi-trash px-[10px] cursor-pointer hover:text-[#c21f0d] transition-colors"></i>
+          <i class="pi pi-times-circle  hover:text-[#c21f0d] transition-colors cursor-pointer"></i>
+        </template>
         </AppDataTable>
         
      </div>
@@ -79,11 +82,31 @@ import { ref } from 'vue';
 import { Button } from 'primevue';
 import AppTitle from '@/core/components/AppTitle.vue';
 import AppModal from '@/core/components/AppModal.vue';
-const showModal = ref<boolean>(false);
+const showModal= ref<boolean>(false);
+const isMode = ref<boolean>(false);
+const tempCountry = ref({
+  nombre: '',
+  abreviación: '',
+  código: ''
+});
 
-const openModalCreate = (value: boolean) => {
-  showModal.value = !value;
-};
+const openModal = (type: 'create' | 'edit', country?: any) => {
+  if (type === 'create') {
+    isMode.value = false;
+    tempCountry.value = { nombre: '', abreviación: '', código: '' };
+    showModal.value = true;
+  } else {
+    isMode.value = true;
+    // Rellena con los datos del país a editar
+    tempCountry.value = {
+      nombre: country.vinculador_dos,
+      abreviación: country.vinculador_tres,
+      código: country.vinculador_cuatro
+    };
+    showModal.value = true;
+  }
+}
+
 
 const closeModalCreate = (value: boolean) => {
   showModal.value = value;
@@ -146,7 +169,7 @@ const items = ref([
     vinculador_tres: 'ESA',
     vinculador_cuatro: '503',
     vinculador_cinco: 'Activo',
-    vinculador_seis: 'Editar | Eliminar',
+    vinculador_seis: 'Editar | Eliminar'
   },
   {
     vinculador_uno: '2',
@@ -154,7 +177,7 @@ const items = ref([
     vinculador_tres: 'GTM',
     vinculador_cuatro: '502',
     vinculador_cinco: 'Activo',
-    vinculador_seis: 'Editar | Eliminar',
+    vinculador_seis: 'Editar | Eliminar'
   },
 ]);
 
