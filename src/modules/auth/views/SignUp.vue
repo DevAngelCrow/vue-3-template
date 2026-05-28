35931f97bd59a2bfc1f21870ef14b34b2eb6996c
angel_dev <angelguille1@hotmail.com>
2026-05-27 22:00:16 -0600

    fix(SignUp): solventa llamada duplicada del servicio de sign-up

--- src/modules/auth/views/SignUp.vue
+++ src/modules/auth/views/SignUp.vue
@@ -95,7 +95,6 @@
               :disabled="isLoading"
               class="w-full mt-1"
               size="large"
-              @click="registerUser({ email, password })"
             >
               <template #default>
                 <span class="flex items-center justify-center gap-2">
